076292ade0890a783a58a9b8471c6e9af4b4764f
Yosua <yosuammwowor@gmail.com>
2025-08-26 19:44:12 +0800

    feat: receive feedback function

--- greetings.go
+++ greetings.go
@@ -5,3 +5,8 @@ import "fmt"
 func SayHello() {
 	fmt.Println("Hello There!");
 }
+
+func ReceiveFeedback(res string) {
+	fmt.Println("Your opinion: ", res);
+	fmt.Println("Thanks for your feedback we'll fix it as soon as possible! 🙌");
+}
